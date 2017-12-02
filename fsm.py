8d714e0caad30c3f88ff1e3f7c2b3eb227fd5139
Chetan Mandloi <chetan.mandloi@live.com>
2017-12-03 00:43:57 +0530

    First complete fsm commit

--- fsm.py
+++ fsm.py
@@ -1,12 +1,16 @@
 import time
 import googlemaps
-handler = googlemaps.Client("AIzaSyAH3qRKfcvNK-Jv3BFj3Rm2ggs91ElQOjE")
 
+SIGNAL_SWITTCH_TIME = 10
+SPEED_LIMIT = 75
+
+
+handler = googlemaps.Client("AIzaSyAH3qRKfcvNK-Jv3BFj3Rm2ggs91ElQOjE")
+epochstart = time.time()
 class Signal_sim:
     def __init__(self, t_step ,num_gates=4):
         self.t_step = t_step
         self.gates = [True]
-        self.last_update_time =  time.hepochstart = time.time()
 
         for i in range(num_gates-1):
             self.gates.append(False)
@@ -16,13 +20,29 @@ class Signal_sim:
                 self.gates[i+1] = True
                 self.gates[i] = False
                 break
+    def get_status(self):
+        curr_time = time.time()
+        rectified = (curr_time - epochstart) % SIGNAL_SWITTCH_TIME * 4
+        if rectified < SIGNAL_SWITTCH_TIME:
+            return True
+        else:
+            return False
+    def time_to_next_green(self):
+        curr_time = time.time()
+        rectified = (curr_time - epochstart) % SIGNAL_SWITTCH_TIME * 4
+        return 4 * SIGNAL_SWITTCH_TIME - rectified
 
 intersection1 = Signal_sim(10)
 
-def Get_Speed_Stat(curr_lat,curr_long,dest_lat,dest_long):
-    
-
-while True:
-    
-    
+def get_speed_stat(curr_lat,curr_long,dest_lat,dest_long):
+    itenary = handler.directions((curr_lat,curr_long),(dest_lat,dest_long))
+    legss = itenary[0]['legs'][0]['steps']
+    total_dist = 0
+    for i in range(len(legss)):
+        total_dist = total_dist + legss[i]['distance']['value']
+    time_left = intersection1.time_to_next_green()
+    speed = total_dist/time_left
+    while(speed>SPEED_LIMIT):
+        time_left = time_left + 40
+        speed == total_dist/time_left
     
