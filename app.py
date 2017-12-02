bec01314357b0317d6b788aa63f1fa57d5cb030f
Purusharth Saxena <saxena.puru005@gmail.com>
2017-12-02 23:31:24 +0530

    Removed redundency files/code

--- app.py
+++ app.py
@@ -4,39 +4,6 @@ import random
 from flask import Flask, jsonify
 app = Flask(__name__)
 
-'''
-tasks = [
-    {
-        'id': 1,
-        'title': u'something',
-        'description': u'APple, banana, pomogranade, kiwi', 
-        'done': True
-    },
-    {
-        'id': 2,
-        'title': u'Something more',
-        'description': u'Loreum Ipsum', 
-        'done': False
-    }
-]
-
-
-@app.route('/')
-@app.route('/index')
-def index():
-    return "Hello, World!"
-
-@app.route('/todo/api/v1.0/tasks', methods=['GET'])
-def get_tasks():
-    return jsonify({'tasks': tasks})
-
-@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
-def get_task(task_id):
-    task = [task for task in tasks if task['id'] == task_id]
-    if len(task) == 0:
-        abort(404)
-    return jsonify({'task': task[0]})
-'''
 
 @app.route('/api/v1.0/<float:lat>@<float:longt>|<float:dlat>@<float:dlongt>', methods=['GET'])
 def get_coordinate(lat, longt, dlat, dlongt):
