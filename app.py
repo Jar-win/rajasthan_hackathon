efc99f9a74876a5c05ff30cc7ccba683bc8db3bd
Purusharth Saxena <saxena.puru005@gmail.com>
2017-12-02 22:32:47 +0530

    added destination

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 #!/usr/bin/ python
 from flask import abort
-
+import random
 from flask import Flask, jsonify
 app = Flask(__name__)
 
-
+'''
 tasks = [
     {
         'id': 1,
@@ -36,11 +36,23 @@ def get_task(task_id):
     if len(task) == 0:
         abort(404)
     return jsonify({'task': task[0]})
+'''
 
-
-@app.route('/api/v1.0/<float:lat>@<float:longt>', methods=['GET'])
-def get_coordinate(lat, longt):
-    return jsonify({'speed': 45.0})
+@app.route('/api/v1.0/<float:lat>@<float:longt>|<float:dlat>@<float:dlongt>', methods=['GET'])
+def get_coordinate(lat, longt, dlat, dlongt):
+    """
+    import direction_handler
+    direction_json = direction_handler.get_direction_json(lat, longt, dlat, dlongt)
+    for step in direction_json['legs'][0]['steps']:
+      try:
+        if step['maneuver'] == 'turn-left' or step['maneuver'] == 'turn-slight-left':
+            continue
+        get_speed_stat(step)
+      except (KeyError, IndexError) as e:
+            continue
+    """
+    random_speed = random.randint(5,80)
+    return jsonify({'speed': random_speed, "callback_time":random_speed})
 
 if __name__ == '__main__':
     app.run(debug=True)
