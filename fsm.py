d9bb2891eb9fcab9a78ef37d29afb002ca7cd934
Chetan Mandloi <chetan.mandloi@live.com>
2017-12-02 23:29:49 +0530

    Made progress on fsm and notebook

--- fsm.py
+++ fsm.py
@@ -1,9 +1,13 @@
 import time
+import googlemaps
+handler = googlemaps.Client("AIzaSyAH3qRKfcvNK-Jv3BFj3Rm2ggs91ElQOjE")
 
 class Signal_sim:
     def __init__(self, t_step ,num_gates=4):
         self.t_step = t_step
         self.gates = [True]
+        self.last_update_time =  time.hepochstart = time.time()
+
         for i in range(num_gates-1):
             self.gates.append(False)
     def switch(self):
@@ -14,4 +18,11 @@ class Signal_sim:
                 break
 
 intersection1 = Signal_sim(10)
+
+def Get_Speed_Stat(curr_lat,curr_long,dest_lat,dest_long):
+    
+
+while True:
+    
+    
     
